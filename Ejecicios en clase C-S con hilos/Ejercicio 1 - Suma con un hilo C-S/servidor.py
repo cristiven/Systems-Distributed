39aeacc3aef6d3b1f4d8a23ad022ea5b1f035658
cristiven <cristiven@utp.edu.co>
2017-03-10 11:08:43 -0500

    Se pasa la funcion de la suma dentro de la clase MyThead

--- Ejecicios en clase C-S con hilos/Ejercicio 1 - Suma con un hilo C-S/servidor.py
+++ Ejecicios en clase C-S con hilos/Ejercicio 1 - Suma con un hilo C-S/servidor.py
@@ -3,19 +3,22 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 import threading
 
 
-def suma_remota(a,b):
-   return a+b
+#def suma_remota(a,b):
+   #return a+b
 
 class MyThread(threading.Thread):
    
     def __init__(self):
         super(MyThread, self).__init__()
+
+    def suma_remota(self,x,y):
+       return x + y
         
     def run(self):
         #se crea el servidor
-        conexion = SimpleXMLRPCServer(("192.168.60.41",8000))
+        conexion = SimpleXMLRPCServer(("localhost",8000))
         print "Amigo soy el servidor... escuchando por el puerto: 8000 "
-        conexion.register_function(suma_remota,"suma")
+        conexion.register_function(self.suma_remota,"suma")
 		# se lanza el servidor
         conexion.serve_forever()
 	
